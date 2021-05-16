e539c4a16d327831ad9bb39356dc8c165adc0eaf
= <=>
2021-05-16 23:05:43 +0500

    Loaded emojies to DB

--- app.js
+++ app.js
@@ -1,11 +1,29 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const MongoClient = require("mongodb").MongoClient;
+const uri = "mongodb+srv://mongo:mongo@emojisdb.6iax4.mongodb.net/emojisdb?retryWrites=true&w=majority";
+const mongoClient = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+const fs = require('fs');
+let dbClient;
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 });
 
+mongoClient.connect(function(err, client){
+    if(err) return console.log(err);
+    let collection = client.db("emojisdb").collection("emojis");
+    collection.find({}, (err, res) => {
+    	res.toArray().then((res) => {
+    		console.log(res);
+    	});
+    });
+
+    app.listen(port, function(){
+        console.log("Сервер ожидает подключения...");
+    });
+});
 
 app.get('/get_emojies', (req, res) => {
 	res.setHeader('Access-Control-Allow-Origin', '*');
@@ -17,6 +35,18 @@ app.get('/get_emojies', (req, res) => {
 	res.send("New");
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}!`)
-});
+
+
+
+function loadEmojiesToDB(collection){
+	let emojis = [];
+	try {
+	  const data = fs.readFileSync('emojis.json', 'utf8');
+	  let jsonData = JSON.parse(data);
+	  emojis = jsonData['emojis'];
+	} catch (err) {
+	  console.error(err)
+	}
+
+	collection.insertMany(emojis);
+}
